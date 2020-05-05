eae2fc9c0a1b37e9cd6ad586e11e5975bb7871f9
ABDUtestSCRIPTS <64821479+ABDUtestSCRIPTS@users.noreply.github.com>
2020-05-05 08:54:20 +0530

    Add files via upload

---
+++ pdf converter.py
@@ -0,0 +1,10 @@
+from docx2pdf import convert
+
+convert(r"C:\Users\Dell\Desktop\data.docx",
+        r"C:\Users\Dell\Desktop\data.pdf")
+        
+def Credit():
+	Space(9); print "############################################"
+	Space(9); print "#   +++ Convert Word Document to PDF +++   #"
+	Space(9); print "#          Script by WH173 5P1D3R          #"
+	Space(9); print "############################################"        
